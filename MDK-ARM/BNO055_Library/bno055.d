88dd8da4c544cdd3879f670dec63637044fede7a
Justrezy007 <joinreyhan@gmail.com>
2022-12-29 14:21:58 +0700

    First Update

---
+++ MDK-ARM/BNO055_Library/bno055.d
@@ -0,0 +1,6 @@
+bno055_library\bno055.o: ..\bno055.c
+bno055_library\bno055.o: ..\bno055.h
+bno055_library\bno055.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdbool.h
+bno055_library\bno055.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+bno055_library\bno055.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
+bno055_library\bno055.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
